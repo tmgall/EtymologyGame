9b12ed47ccb24dc9542e01915eee22e97871d5be
Tyler Gall <tmgall@outlook.com>
2025-03-09 22:55:26 -0700

    Add contact

--- src/support/Contact.tsx
+++ src/support/Contact.tsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div className="bg-sky-950 flex flex-col items-center space-y-4 p-4 min-h-screen">
-      <h1 className="text-2xl font-bold text-sky-100">Contact</h1>
-      <p className="text-sky-100">This is where your contact goes.</p>
+    <div className="min-h-dvh bg-sky-950">
+      <div className="max-w-96 w-full mx-auto flex flex-col items-center space-y-4 p-4 text-center">
+        <h1 className="text-2xl font-bold text-sky-100">Contact</h1>
+        <p className="text-sky-100">The developer hopes you like the game! For any questions or feedback, please contact: </p>
+        <p className="text-sky-100">Email: <a href="mailto:lexicongamecontact@gmail.com" className="text-sky-100 hover:underline">lexicongamecontact@gmail.com</a></p>
 
-      <Link to="/" className="text-sky-100 hover:underline">
-        Back to Home
-      </Link>
+        <Link to="/" className="text-sky-100 hover:underline">
+          Back to Home
+        </Link>
+      </div>
     </div>
   );
 };
