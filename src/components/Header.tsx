e80ef0b36b892cfb03f31f19eaa1f9f97d5048d8
Tyler Gall <tmgall@outlook.com>
2025-04-01 19:56:47 -0700

    Migrate header to CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,23 +7,15 @@ export interface HeaderProps {
 
 const Header = ({ setIsHelpModalOpen, setIsSuccessModalOpen }: HeaderProps) => {
     return (
-        <div className="flex justify-between items-center w-full">
-            <div>
-                <div className="text-5xl font-bold font-mono max-w-96 self-starts text-sky-100">lexicon</div>
-                <div className="max-w-96 font-mono self-start text-sky-100">{getFormattedDate()}: Puzzle #{getPuzzleNumber()}</div>
+        <div className="header">
+            <div className="headerText">
+                <div className="headerName">lexicon</div>
+                <div className="headerPuzzleNumber">{getFormattedDate()}: Puzzle #{getPuzzleNumber()}</div>
             </div>
-            <button
-                className="text-xl font-mono font-bold text-sky-100 bg-sky-700 rounded-full w-6 h-6 flex items-center justify-center hover:bg-sky-800 cursor-pointer"
-                onClick={() => setIsSuccessModalOpen(true)}
-            >
-                ^
-            </button>
-            <button
-                className="text-xl font-mono font-bold text-sky-100 bg-sky-700 rounded-full w-6 h-6 flex items-center justify-center hover:bg-sky-800 cursor-pointer"
-                onClick={() => setIsHelpModalOpen(true)}
-            >
-                ?
-            </button>
+            <div className="headerButtons">
+                <button className="headerButton" onClick={() => setIsSuccessModalOpen(true)}>#</button>
+                <button className="headerButton" onClick={() => setIsHelpModalOpen(true)}>?</button>
+            </div>
         </div>
     );
 }
