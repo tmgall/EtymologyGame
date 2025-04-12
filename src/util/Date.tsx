457c6e0b907912f197b0aae3e2d67480fcfdce77
Tyler Gall <tmgall@outlook.com>
2025-04-12 13:08:36 -0700

    More CSS improvements

--- src/util/Date.tsx
+++ src/util/Date.tsx
@@ -2,7 +2,7 @@
 export const getFormattedDate = () => {
     const today = new Date();
     const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     const dayOfWeek = dayNames[today.getDay()];
     const dayOfMonth = today.getDate();
     const month = monthNames[today.getMonth()];
