1bbb5f127f92e8c631fbcc297ffd2edbb423149f
Tyler Gall <tmgall@outlook.com>
2025-04-27 17:50:34 -0700

    Add new words

--- src/assets/WordList.tsx
+++ src/assets/WordList.tsx
@@ -3,7 +3,6 @@ export interface WordData {
     answer: string;
     roots: RootsData[];
     definition: string;
-    difficulty: string;
     authorsNote?: string;
 }
 
@@ -13,145 +12,221 @@ export interface RootsData {
     languageWord: string;
 }
 
+// 3 easy, 2 medium, 2 hard per week
 export const WORD_LIST: WordData[] = [
-    { 
+    // New week
+    {
         clue: "far vision", 
         answer: "television", 
         roots: [{ english: "far", languageName: "Greek", languageWord: "tele-" }, { english: "vision", languageName: "Latin", languageWord: "visio" }],
         definition: "technology with a screen",
-        difficulty: "Easy",
     },
     { 
         clue: "life writing", 
         answer: "biography", 
         roots: [{ english: "life", languageName: "Greek", languageWord: "bios" }, { english: "writing", languageName: "Greek", languageWord: "-graphia" }],
         definition: "an account of someone's life",
-        difficulty: "Easy",
-    },
-    { 
-        clue: "spiral wing", 
-        answer: "helicopter", 
-        roots: [{ english: "spiral", languageName: "Greek", languageWord: "bios" }, { english: "wing", languageName: "Greek", languageWord: "pteron" }],
-        definition: "a type of aircraft",
-        difficulty: "Medium",
-    },
-    { 
-        clue: "lord of the world", 
-        answer: "juggernaut", 
-        roots: [{ english: "the world", languageName: "Sanskrit", languageWord: "jagat" }, { english: "natha", languageName: "Sanskrit", languageWord: "lord" }],
-        definition: "a huge or overwhelming force",
-        difficulty: "Hard",
-    },
-    {
-        clue: "small-seeing instrument",
-        answer: "microscope", 
-        roots: [{ english: "small", languageName: "Latin", languageWord: "micro-" }, { english: "seeing instrument", languageName: "Latin", languageWord: "-scopium" }],
-        definition: "an instrument to see very small objects",
-        difficulty: "Easy",
-    },
-    {
-        clue: "measurement of three angles",
-        answer: "trigonometry",
-        roots: [{ english: "three", languageName: "Greek", languageWord: "tri"}, { english: "angles", languageName: "Greek", languageWord: "gonia"}, { english: "measurement", languageName: "Greek", languageWord: "metron"}],
-        definition: "a branch of math",
-        difficulty: "Easy",
-    },
-    {
-        clue: "harbor wave",
-        answer: "tsunami", 
-        roots: [{ english: "harbor", languageName: "Japanese", languageWord: "tsu" }, { english: "wave", languageName: "Japanese", languageWord: "nami" }],
-        definition: "a big wave",
-        difficulty: "Medium",
     },
     {
         clue: "start from",
         answer: "originate", 
         roots: [{ english: "beginning, source", languageName: "Latin", languageWord: "originem" }],
         definition: "begin at",
-        difficulty: "Easy",
+    },
+    { 
+        clue: "spiral wing", 
+        answer: "helicopter", 
+        roots: [{ english: "spiral", languageName: "Greek", languageWord: "bios" }, { english: "wing", languageName: "Greek", languageWord: "pteron" }],
+        definition: "a type of aircraft",
+    },
+    {
+        clue: "harbor wave",
+        answer: "tsunami", 
+        roots: [{ english: "harbor", languageName: "Japanese", languageWord: "tsu" }, { english: "wave", languageName: "Japanese", languageWord: "nami" }],
+        definition: "a big wave",
+    },
+    { 
+        clue: "lord of the world", 
+        answer: "juggernaut", 
+        roots: [{ english: "the world", languageName: "Sanskrit", languageWord: "jagat" }, { english: "lord", languageName: "Sanskrit", languageWord: "natha" }],
+        definition: "a huge or overwhelming force",
     },
     {
         clue: "joy from harm",
         answer: "Schadenfreude", 
         roots: [{ english: "harm", languageName: "German", languageWord: "Schaden" }, { english: "joy", languageName: "German", languageWord: "Freude" }],
         definition: "joy from harm to others",
-        difficulty: "Hard",
     },
+    // New week
     {
-        clue: "two wheels",
-        answer: "bicycle", 
-        roots: [{ english: "two", languageName: "Greek", languageWord: "bi-" }, { english: "wheel", languageName: "Greek", languageWord: "kyklos" }],
-        definition: "a two-wheeled transport",
-        difficulty: "Easy",
+        clue: "small-seeing instrument",
+        answer: "microscope", 
+        roots: [{ english: "small", languageName: "Latin", languageWord: "micro-" }, { english: "seeing instrument", languageName: "Latin", languageWord: "-scopium" }],
+        definition: "an instrument to see very small objects",
     },
     {
         clue: "flesh devourer",
         answer: "carnivore",
         roots: [{ english: "flesh", languageName: "Latin", languageWord: "carni" }, { english: "devour", languageName: "Latin", languageWord: "vore" }],
         definition: "one that eats meat",
-        difficulty: "Easy",
     },
     {
-        clue: "beyond reality",
-        answer: "surrealism", 
-        roots: [{ english: "beyond", languageName: "French", languageWord: "sur" }, { english: "reality", languageName: "French", languageWord: "realisme" }],
-        definition: "bizarre and dreamlike art",
-        difficulty: "Easy",
-    },
-    {
-        clue: "star sailor",
-        answer: "astronaut", 
-        roots: [{ english: "star", languageName: "Greek", languageWord: "astro" }, { english: "sailor", languageName: "Greek", languageWord: "naut" }],
-        definition: "one exploring space",
-        difficulty: "Easy",
-    },
-    {
-        clue: "empty orchestra",
-        answer: "karaoke", 
-        roots: [{ english: "empty", languageName: "Japanese", languageWord: "kara" }, { english: "orchestra", languageName: "Japanese", languageWord: "oke" }],
-        definition: "singing with just the music",
-        difficulty: "Medium",
-    },
-    {
-        clue: "one marriage",
-        answer: "monogamy",
-        roots: [{ english: "one", languageName: "Greek", languageWord: "monos"}, { english: "marriage", languageName: "Greek", languageWord: "gamos"}],
-        definition: "one marriage at one time",
-        difficulty: "Easy",
-    },
-    {
-        clue: "Sax's sound",
-        answer: "saxophone",
-        roots: [{ english: "Sax (surname)", languageName: "French", languageWord: "Sax"}, { english: "sound", languageName: "Greek", languageWord: "phone"}],
-        definition: "type of woodwind instrument",
-        difficulty: "Easy",
-    },
-    {
-        clue: "forbidden",
-        answer: "taboo",
-        roots: [{ english: "forbidden", languageName: "Tongan", languageWord: "tapu"}],
-        definition: "socially unacceptable",
-        difficulty: "Medium",
-    },
-    {
-        clue: "fear of spiders",
-        answer: "arachnophobia",
-        roots: [{ english: "spider", languageName: "Greek", languageWord: "arakhne"}, { english: "fear", languageName: "Greek", languageWord: "phobia"}],
-        definition: "literally fear of spiders",
-        difficulty: "Easy",
-    },
-    {
-        clue: "journey",
-        answer: "safari",
-        roots: [{ english: "journey", languageName: "Arabic", languageWord: "safar"}],
-        definition: "expedition to hunt in Africa",
-        difficulty: "Hard",
+        clue: "measurement of three angles",
+        answer: "trigonometry",
+        roots: [{ english: "three", languageName: "Greek", languageWord: "tri"}, { english: "angles", languageName: "Greek", languageWord: "gonia"}, { english: "measurement", languageName: "Greek", languageWord: "metron"}],
+        definition: "a branch of math",
     },
     {
         clue: "running back again",
         answer: "palindrome",
         roots: [{ english: "again", languageName: "Greek", languageWord: "palin"}, { english: "running", languageName: "Greek", languageWord: "dromos"}],
         definition: "a word the same backwards",
-        difficulty: "Medium",
-    }
+    },
+    {
+        clue: "forbidden",
+        answer: "taboo",
+        roots: [{ english: "forbidden", languageName: "Tongan", languageWord: "tapu"}],
+        definition: "socially unacceptable",
+    },
+    {
+        clue: "journey",
+        answer: "safari",
+        roots: [{ english: "journey", languageName: "Arabic", languageWord: "safar"}],
+        definition: "expedition to hunt in Africa",
+    },
+    {
+        clue: "army cry",
+        answer: "slogan",
+        roots: [{ english: "army", languageName: "Gaelic", languageWord: "slua"}, { english: "cry", languageName: "Gaelic", languageWord: "ghairm"}],
+        definition: "a memorable phrase or motto"
+    },
+    // New week
+    {
+        clue: "two wheels",
+        answer: "bicycle", 
+        roots: [{ english: "two", languageName: "Greek", languageWord: "bi-" }, { english: "wheel", languageName: "Greek", languageWord: "kyklos" }],
+        definition: "a two-wheeled transport",
+    },
+    {
+        clue: "star sailor",
+        answer: "astronaut", 
+        roots: [{ english: "star", languageName: "Greek", languageWord: "astro" }, { english: "sailor", languageName: "Greek", languageWord: "naut" }],
+        definition: "one exploring space",
+    },
+    {
+        clue: "beyond reality",
+        answer: "surrealism", 
+        roots: [{ english: "beyond", languageName: "French", languageWord: "sur" }, { english: "reality", languageName: "French", languageWord: "realisme" }],
+        definition: "bizarre and dreamlike art",
+    },
+    {
+        clue: "Sax's sound",
+        answer: "saxophone",
+        roots: [{ english: "Sax (surname)", languageName: "French", languageWord: "Sax"}, { english: "sound", languageName: "Greek", languageWord: "phone"}],
+        definition: "type of woodwind instrument",
+    },
+    {
+        clue: "empty orchestra",
+        answer: "karaoke", 
+        roots: [{ english: "empty", languageName: "Japanese", languageWord: "kara" }, { english: "orchestra", languageName: "Japanese", languageWord: "oke" }],
+        definition: "singing with just the music",
+    },
+    {
+        clue: "fast walker",
+        answer: "giraffe",
+        roots: [{english: "fast walker",languageName: "Arabic",languageWord: "zarafa"}],
+        definition: "a tall, long-necked animal"
+    },
+    {
+        clue: "know apart",
+        answer: "Diagnosis",
+        roots: [{english: "apart",languageName: "Greek",languageWord: "dia"},{english: "knowledge",languageName: "Greek",languageWord: "gnosis"}],
+        definition: "identification of a condition or problem"
+    },
+    // New week
+    {
+        clue: "one marriage",
+        answer: "monogamy",
+        roots: [{ english: "one", languageName: "Greek", languageWord: "monos"}, { english: "marriage", languageName: "Greek", languageWord: "gamos"}],
+        definition: "one marriage at one time",
+    },
+    {
+        clue: "fear of spiders",
+        answer: "arachnophobia",
+        roots: [{ english: "spider", languageName: "Greek", languageWord: "arakhne"}, { english: "fear", languageName: "Greek", languageWord: "phobia"}],
+        definition: "literally fear of spiders",
+    },
+    {
+        clue: "hand writing",
+        answer: "manuscript",
+        roots: [{english: "hand",languageName: "Latin",languageWord: "manus"},{english: "to write",languageName: "Latin",languageWord: "scribere"}],
+        definition: "a handwritten document"
+    },
+    {
+        clue: "self moving",
+        answer: "automaton",
+        roots: [{english: "self",languageName: "Greek",languageWord: "auto"},{english: "moving",languageName: "Greek",languageWord: "matos"}],
+        definition: "a machine that moves by itself"
+    },
+    {
+        clue: "the spirit/essence",
+        answer: "alcohol",
+        roots: [{ english: "the", languageName: "Arabic", languageWord: "al" },{ english: "chemical spirit/essence", languageName: "Arabic", languageWord: "kohl" }],
+        definition: "a drinkable intoxicating liquid"
+    },
+    {
+        clue: "sensing together",
+        answer: "synesthesia",
+        roots: [{ english: "together", languageName: "Greek", languageWord: "syn" }, { english: "sensation", languageName: "Greek", languageWord: "aisthesis" }],
+        definition: "condition where the senses blend together"
+    },
+    {
+        clue: "way of adapting the spirit",
+        answer: "aikido",
+        roots: [{ english: "together", languageName: "Japanese", languageWord: "ai" }, { english: "spirit", languageName: "Japanese", languageWord: "ki" }, { english: "way/path", languageName: "Japanese", languageWord: "do" }],
+        definition: "Japanese martial art focused on harmony"
+    },
+    // New week
+    {
+        clue: "fire mountain",
+        answer: "volcano",
+        roots: [{ english: "fire, burning", languageName: "Latin", languageWord: "vulcanus" }],
+        definition: "a mountain that erupts molten rock"
+    },
+    {
+        clue: "ice hill",
+        answer: "iceberg",
+        roots: [{ english: "ice", languageName: "English", languageWord: "ice" }, { english: "hill/mountain", languageName: "Old Norse", languageWord: "berg" }],
+        definition: "a large floating mass of ice"
+    },
+    {
+        clue: "middle earth",
+        answer: "Mediterranean",
+        roots: [{ english: "middle", languageName: "Latin", languageWord: "medius" }, { english: "earth", languageName: "Latin", languageWord: "terra" }],
+        definition: "region surrounding a large inland sea"
+    },
+    {
+        clue: "house management",
+        answer: "economy",
+        roots: [{ english: "house", languageName: "Greek", languageWord: "oikos" }, { english: "manage/keep", languageName: "Greek", languageWord: "nemein" }],
+        definition: "management of resources or wealth"
+    },
+    {
+        clue: "study of secrets",
+        answer: "cryptology",
+        roots: [{ english: "secret", languageName: "Greek", languageWord: "kryptos" }, { english: "study", languageName: "Greek", languageWord: "logia" }],
+        definition: "study of codes and secret communication"
+    },
+    {
+        clue: "gold flower",
+        answer: "chrysanthemum",
+        roots: [{ english: "gold", languageName: "Greek", languageWord: "khrysos"}, { english: "flower", languageName: "Greek", languageWord: "anthemon"}],
+        definition: "a variety of golden flower"
+    },
+    {
+        clue: "the soda ashes",
+        answer: "alkaline",
+        roots: [{ english: "the", languageName: "Arabic", languageWord: "al"},{ english: "ashes of saltwort", languageName: "Arabic", languageWord: "qaly" }],
+        definition: "having a high pH"
+    },
 ];
