0bcc1a4315d41ce6c0e2e329771a4b262b15ce14
Tyler Gall <tmgall@outlook.com>
2025-05-06 19:44:00 -0700

    Fix spacing 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import Puzzle from "./components/Puzzle";
 import { getTodaysPuzzleNumber } from "./util/Date";
 
 const App = () => {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+  
+    setVh();
+    window.addEventListener('resize', setVh); // Update on resize
+  
+    return () => window.removeEventListener('resize', setVh);
+  }, []);
+  
   const override = undefined;
   const puzzleNumber = override ?? getTodaysPuzzleNumber();
   return (
