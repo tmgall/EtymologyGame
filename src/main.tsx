54df1fa3b3867bab3ac1cddf0fe5f586f5241e18
Tyler Gall <tmgall@outlook.com>
2025-11-16 14:51:27 -0800

    Fix init db

--- src/main.tsx
+++ src/main.tsx
@@ -10,19 +10,19 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { initDB } from './util/db.ts';
 
-await initDB();
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-of-service" element={<TermsOfService />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-    <Analytics />
-    <SpeedInsights />
-  </StrictMode>,
-)
+initDB().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Analytics />
+      <SpeedInsights />
+    </StrictMode>,
+  )
+});
