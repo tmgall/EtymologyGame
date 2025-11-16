b45afc0e5a6280704ef6a7c2480a719e6bc06597
Tyler Gall <tmgall@outlook.com>
2025-11-16 15:13:34 -0800

    Update date

--- src/util/Date.tsx
+++ src/util/Date.tsx
@@ -1,4 +1,4 @@
-const LAUNCH_DATE = new Date("November 10, 2025 00:00:00");
+const LAUNCH_DATE = new Date("November 17, 2025 00:00:00");
 
 export const getFormattedDate = (puzzleNumber: string) => {
     const puzzleDate = new Date(LAUNCH_DATE);
